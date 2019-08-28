325e1882aa1619750d7f1e63233c130cc9e58408
Henrique Rocha <hmrocha@gmail.com>
2019-08-28 16:55:11 +0200

    Add Chronometer.

--- app/src/main/java/me/henriquerocha/android/stopwatch/MainActivity.java
+++ app/src/main/java/me/henriquerocha/android/stopwatch/MainActivity.java
@@ -1,7 +1,48 @@
 package me.henriquerocha.android.stopwatch;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class MainActivity extends Activity {
 
+    private long base;
+    private boolean started;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Chronometer chronometer = findViewById(R.id.chronometer);
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (started) {
+                    chronometer.setBase(base);
+                } else {
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                }
+                chronometer.start();
+                started = true;
+            }
+        });
+
+        Button stopButton = findViewById(R.id.stopButton);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long b = chronometer.getBase();
+                base = b + (SystemClock.elapsedRealtime() - b);
+
+                chronometer.stop();
+            }
+        });
+    }
 }
